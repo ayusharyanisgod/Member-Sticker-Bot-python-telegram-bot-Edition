9ccae5ca5c6b3ec731e4190c1c186ec88e1fb4e3
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-05-24 17:56:42 +0530

    Update others.py

--- tg_bot/modules/others.py
+++ tg_bot/modules/others.py
@@ -1,12 +1,12 @@
 __help__ = """
-◆ Bot Name : *Welcome Adder*
+◆ Bot Name : *Member Sticker Bot*
 ◆ Creator : [BUGHUNTER](http://t.me//bughunter0)
-◆ Credits :  Myself
+◆ Credits :  All Along the Journey
 ◆ Language : Python3
 ◆ Source Code :  [BUGHUNTERBOTS](https://t.me/bughunterbots)
 ◆ Server : Heroku
 ◆ Database : Postgre SQL
-◆ Build Status : stable (beta 2.58)
-◆ New feature : Random Welcome / Goodbye Message 
+◆ Build Status : stable (beta 1.0)
+◆ New feature : Sticker added Upto 2500 members
 """
 __mod_name__ = "About"

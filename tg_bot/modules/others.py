043544cdc07557e12eda8d08b3a89ddb40df93e0
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-05-24 08:31:19 +0530

    Add files via upload

---
+++ tg_bot/modules/others.py
@@ -0,0 +1,12 @@
+__help__ = """
+◆ Bot Name : *Welcome Adder*
+◆ Creator : [BUGHUNTER](http://t.me//bughunter0)
+◆ Credits :  Myself
+◆ Language : Python3
+◆ Source Code :  [BUGHUNTERBOTS](https://t.me/bughunterbots)
+◆ Server : Heroku
+◆ Database : Postgre SQL
+◆ Build Status : stable (beta 2.58)
+◆ New feature : Random Welcome / Goodbye Message 
+"""
+__mod_name__ = "About"

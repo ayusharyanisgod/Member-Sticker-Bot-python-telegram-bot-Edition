44ca848c4739b21827617503c3e2d1f21d31a581
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-05-28 08:13:58 +0530

    Update others.py

--- tg_bot/modules/others.py
+++ tg_bot/modules/others.py
@@ -1,13 +1,13 @@
 __help__ = """
 ◆ Bot Name : *Member Sticker Bot*
 ◆ Creator : [BUGHUNTER](http://t.me//bughunter0)
-◆ Credits :  All Along the Journey
+◆ Credits :  [All Mentioned Here](https://github.com/bughunter0/member-sticker-bot#credits)
 ◆ Language : Python3
 ◆ Source Code :  [BUGHUNTERBOTS](https://t.me/bughunterbots)
 ◆ Server : Heroku
 ◆ Database : Postgre SQL
-◆ Build Status : stable (beta 1.0)
-◆ New feature : Sticker added Upto 25000 members
+◆ Build Status : stable (2.3)
+◆ New feature : Sticker added Upto 100000 members
 """
 __mod_name__ = "About"
 
